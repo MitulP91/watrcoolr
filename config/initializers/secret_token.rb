e70dc16ace75fc866b1677bd0580e43e54bfe065
MitulP91 <mitulp91@gmail.com>
2014-05-03 13:36:22 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Watrcoolr::Application.config.secret_key_base = 'bee5a21bc1bd3b7af19b76f35c241c6ef435b11b4e207c57563e51e12461324e290d536af4939874e8954af7cb8d8ed31abbefaa027d98faac734ebb086d6469'
