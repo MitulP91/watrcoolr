72b124d75be9c873402ad87b94f3e291eaec2353
MitulP91 <mitulp91@gmail.com>
2014-05-03 17:15:08 -0400

    add messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,26 @@
 class MessagesController < ApplicationController
+  before_action :set_type
+
+  def index
+    @messages = type_class.all
+  end
+
+  # Starts private functions
+  private
+
+  def set_type
+    @type = type
+  end
+
+  def type
+    params[:type] || "Message"
+  end
+
+  def type_class
+    type.constantize
+  end
+
+  def set_message
+    @message = type_class.find(params[:id])
+  end
 end
